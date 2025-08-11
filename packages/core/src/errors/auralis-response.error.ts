5c1833781bd84020a6458b69ba4c47025d5b26e5
Shinigami92 <chrissi92@hotmail.de>
2025-08-11 22:14:09 +0200

    infra: format

--- packages/core/src/errors/auralis-response.error.ts
+++ packages/core/src/errors/auralis-response.error.ts
@@ -1,4 +1,4 @@
-import type { ServerResponse, IncomingMessage } from "node:http";
+import type { IncomingMessage, ServerResponse } from "node:http";
 
 export abstract class AuralisResponseError extends Error {
   statusCode: number;
